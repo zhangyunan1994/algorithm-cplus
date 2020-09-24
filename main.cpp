5a8fcc66ef9fa6e76fd21ed3dac4187e7ffb8c92
zhangyunan <zyndev@gmail.com>
2020-09-24 22:52:31 +0800

    add fib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+using namespace std;
+
+int fib(int N) {
+    if(N < 2) {
+        return N;
+    }
+    if(N == 2) {
+        return 1;
+    }
+    int a = 0, b = 1, result=0;
+    for(int i=2; i<=N; ++i) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
+}
+
+int fibRecursion(int N) {
+    if (N < 2) {
+        return N;
+    } else if (N == 2) {
+        return 1;
+    } else {
+        return fibRecursion(N - 1) + fibRecursion(N-2);
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    for (int i = 0; i < 10; ++i) {
+        cout << fibRecursion(i) << endl;
+    }
+
 }
